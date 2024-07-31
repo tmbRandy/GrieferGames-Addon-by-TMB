a6ba931d1b5988ca1f25a9ccf745ef9b071605a5
tmbRandy <frappe-scherze.04@icloud.com>
2024-07-31 16:34:53 +0200

    Updated AccountUnity

--- game-runner/src/v1_8_9/java/tmb/randy/tmbgriefergames/v1_8_9/util/AccountUnity.java
+++ game-runner/src/v1_8_9/java/tmb/randy/tmbgriefergames/v1_8_9/util/AccountUnity.java
@@ -1,17 +1,17 @@
 package tmb.randy.tmbgriefergames.v1_8_9.util;
 
-import net.labymod.accountmanager.storage.account.Account;
+import net.labymod.api.Laby;
+import net.labymod.api.account.Account;
 import net.labymod.api.event.client.chat.ChatReceiveEvent;
-// Unfortunately there is no LabyAPI for this so we must use the Core package :/
-import net.labymod.core.main.LabyMod;
 
 public class AccountUnity {
 
     public void messageReceived(ChatReceiveEvent event) {
-        for (Account account : LabyMod.getInstance().getAccountManager().getAccounts()) {
+        for (Account account : Laby.references().accountService().getAccounts()) {
             String name = account.getUsername();
             if(event.chatMessage().getPlainText().endsWith(name + " möchte sich zu dir teleportieren.") || event.chatMessage().getPlainText().endsWith(name + " möchte, dass du dich zu der Person teleportierst.")) {
                 VersionisedBridge.sendCommand("/tpaccept");
+
             }
         }
     }
