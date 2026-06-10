ff045e590f40fff71bb2785bff184967acef102b
tmbRandy <frappe-scherze.04@icloud.com>
2026-06-10 22:50:14 +0200

    Reverted some changes and added required check sum

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,23 @@
 rootProject.name = "griefergames-addon-by-tmb"
 
 pluginManagement {
-    repositories {
-        maven("https://dist.labymod.net/api/v1/maven/release/")
-        maven("https://maven.neoforged.net/releases/")
-        maven("https://maven.fabricmc.net/")
-        gradlePluginPortal()
-        mavenCentral()
-    }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "net.labymod.labygradle.settings") {
-                useModule("net.labymod.gradle:common:${requested.version}")
-            }
+    val labyGradlePluginVersion = "0.8.1"
+    buildscript {
+        repositories {
+            maven("https://maven.laby.net/api/v1/maven/release/")
+            maven("https://maven.neoforged.net/releases/")
+            maven("https://maven.fabricmc.net/")
+            gradlePluginPortal()
+            mavenCentral()
+        }
+
+        dependencies {
+            classpath("net.labymod.gradle", "common", labyGradlePluginVersion)
         }
     }
 }
 
-plugins {
-    id("net.labymod.labygradle.settings") version "0.6.2"
-}
+plugins.apply("net.labymod.labygradle.settings")
 
 include(":api")
 include(":core")
